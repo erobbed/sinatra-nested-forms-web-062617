0386e8b2c81f9abb5ebf2d626e06566f0e9558d4
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-07-19 14:34:07 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,20 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+    get '/new' do
+      erb :'pirates/new'
+    end
 
+    post '/pirates' do
+      @pirate = Pirate.new(*params[:pirate].reject{|param| param == "ships"}.values)
+      params[:pirate][:ships].each do |ship|
+        Ship.new(*ship.values)
+      end
+      @ships = Ship.all
+      erb :'pirates/show'
+    end
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+  PIRATES = []
+  def initialize(name, weight, height)
+    @name = name
+    @height = height
+    @weight = weight
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
